90c7eaed7f2529a3c31d5a4c137c188bfb99ee55
hennabackman <henna.backman@gmail.com>
2023-01-17 16:12:25 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [text, setText] = useState('');
+
+  const addItem = () => {
+    setItems([...items, { key: items.length.toString(), value: text }]);
+    setText('');
+  };
+
+  const clearList = () => {
+    setItems([]);
+  };
+  const Header = () => {
+    return (
+        <Text style={styles.headerText}>Shopping List</Text>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add item"
+          onChangeText={text => setText(text)}
+          value={text}
+          style={styles.input}
+        />
+        </View>
+        <View style={styles.buttonContainer}>
+        <Button style={styles.button}
+        title="Add" 
+        onPress={addItem} 
+        />
+        <Button style={styles.button}
+        title="Clear" 
+        onPress={clearList} 
+        />
+        </View>
+      <FlatList style={styles.headertext}
+        ListHeaderComponent={<Header/>}
+        data={items}
+        renderItem={({ item }) => <Text style={styles.item}>{item.value}</Text>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 10
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 10
+  },
+  button: {
+    margin: 10,
+    width: '50%',
+    alignSelf: 'center'
+  },
+  headertext: {
+    margin: 20,
+    fontSize: 20,
+    alignSelf:'center'
   },
 });
+export default App;
